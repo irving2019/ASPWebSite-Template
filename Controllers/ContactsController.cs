aef7224284b6639a4a381ebc29f889b413ff6608
Vladimir Pilipenko <vovapilip46@gmail.com>
2025-03-04 19:44:34 +0500

    Добавьте файлы проекта.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPWebSite.Models;
+
+namespace ASPWebSite.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() //Contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact) 
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+
+
+        }
+
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPWebSite.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите Имя")]
+        [Required(ErrorMessage = "Не корректный ввод")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введите Фамилию")]
+        [Required(ErrorMessage = "Не корректный ввод")]
+        public string SurName { get; set; }
+
+        [Display(Name = "Введите Возраст")]
+        [Required(ErrorMessage = "Не корректный ввод")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите e-mail")]
+        [Required(ErrorMessage = "Не корректный ввод")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите Сообщение")]
+        [StringLength(30, ErrorMessage = "Не меньше 30 символов")]
+        [Required(ErrorMessage = "Не корректный ввод")]
+        public string Message { get; set; }
+
+    }
+}
